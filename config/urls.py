2da206dffe76485a6127353efd6bad902f4f9680
bemndy <brandonprvbox@gmail.com>
2026-04-10 03:04:57 -0400

    feat: created two apps, added to settings, configured urls using class-based views, also redirected homepage to other pages depending on auth status of user, committing changes to db

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from game import views as game_views
+from accounts import views as accounts_views
+
+# Using the redirect in game/views.py to check AUTH status and redirect to login if not logged in
+# Also using it for the "" root URL to redirect to /game if logged in, or /login if not logged in 
+
+def home(request):
+    if request.user.is_authenticated:
+        return redirect('game')
+    return redirect('login')
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
+    path('game/', game_views.GameView, name='game'),
+    path('login/', LoginView.as_view(
+        template_name='accounts/login.html', 
+        next_page='/game/',
+        redirect_authenticated_user=True
+    ), name='login'),
+    path('logout/', LogoutView.as_view(
+        template_name='accounts/logout.html',
+    ), name='logout'),
+    path('signup/', accounts_views.RegisterView, name='signup'),
 ]
