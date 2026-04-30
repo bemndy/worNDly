65bdf4fba7346ee4b4042fd1e9a6285229fcfd73
bemndy <brandonprvbox@gmail.com>
2026-04-30 12:39:13 -0400

    fix: changing env to being loaded with os.getenv, so need to add instructions for that in the readme

--- tokens/views.py
+++ tokens/views.py
@@ -8,22 +8,15 @@ from game.views import get_play_bank
 
 # Create your views here.
 
-ACCESS_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzg2MTQzODkzLCJpYXQiOjE3Nzc1MDM4OTMsImp0aSI6IjZjOGUwMGFhNGJmNDQzOTFiOTA5MTQ3Nzc1ZjY1MTMwIiwidXNlcl9pZCI6NTN9.8dsiReyLVmDlGVWW8VQulBfgvxYwpsphCHRtQJ1LFCc'
-@login_required
-def BuyTokensView(request):
-    return render(request, 'tokens/token_balance.html', {
-        'players': view_all_coins(ACCESS_TOKEN),
-        'current_player': view_balance_for_user(
-            ACCESS_TOKEN, 
-            request.user.email)
-    })
+# get token from environment variable
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 
+### HELPERS ###
 def view_all_coins(access_token):
    # Use the access token to make an authenticated request
    headers = {
        'Authorization': f'Bearer {access_token}'
    }
-
 
    # Make a GET request with the authorization header
    api_response = requests.get("https://jcssantos.pythonanywhere.com/api/group22/group22/", headers=headers)
@@ -48,8 +41,6 @@ def view_balance_for_user(access_token, email):
 
    if api_response.status_code == 200:
        return api_response.json()
-   elif api_response.status_code == 404:
-       return user_initialize_balance(access_token, email)
    else:
        print("Failed to access the API endpoint to view balance for user:", api_response.status_code)
 
@@ -91,18 +82,12 @@ def user_pay(access_token, email, amount):
    else:
        print("Failed to access the API endpoint to pay:", api_response.status_code)
 
-def user_initialize_balance(access_token, email):
-   # Use the access token to make an authenticated request
-   headers = {
-       'Authorization': f'Bearer {access_token}'
-   }
-   data = {"amount": 20} # Initialize with zero tokens
-   # Make a POST request with the authorization header and data payload to add a user
-   api_response = requests.post(f"https://jcssantos.pythonanywhere.com/api/group22/group22/player/add", headers=headers, data=data)
-
-
-   if api_response.status_code == 200:
-       # Process the data from the API
-       return api_response.json()
-   else:
-       print("Failed to access the API endpoint to initialize balance:", api_response.status_code)
+### Requests ###
+@login_required
+def BuyTokensView(request):
+    return render(request, 'tokens/token_balance.html', {
+        'players': view_all_coins(ACCESS_TOKEN),
+        'current_player': view_balance_for_user(
+            ACCESS_TOKEN, 
+            request.user.email)
+    })
